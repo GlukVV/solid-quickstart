6b26683ee4cbcf03ea81775ab03bfb95fa15297e
GlukVV <pdmr081@gmail.com>
2023-06-08 16:02:41 +0300

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,6 +8,11 @@ export default function Home() {
       <h1>Hello world!cqweqwqw</h1>
       <Counter />
       <p>
+      <p><a href="https://kd6yl.app.link"><img src="/1_4NhFKMxr-qXodjYpxtiE0w.gif" width="150" 
+    alt=" "></a></p>  
+        
+        
+        
         Visit{" "}
         <a href="https://start.solidjs.com" target="_blank">
           start.solidjs.com
